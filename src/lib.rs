7139c9d508afca906031dc2092c979e8c7881d90
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-07 00:39:09 +0700

    2. Components - Phase 1

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ mod components;
 use components::login::Login;
 use components::chat::Chat;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -18,6 +21,15 @@ use components::chat::Chat;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+pub type User = Rc<UserInner>;
+
+#[derive(Debug, PartialEq)]
+pub struct UserInner {
+    pub username: RefCell<String>,
+}
+
 
 #[derive(Debug, Clone, Copy, PartialEq, Routable)]
 pub enum Route {
@@ -40,12 +52,20 @@ fn switch(selected_route: &Route) -> Html {
 
 #[function_component(Main)]
 fn main() -> Html {
+    let ctx = use_state(|| {
+        Rc::new(UserInner {
+            username: RefCell::new("initial".into()),
+        })
+    });
+
     html! {
+        <ContextProvider<User> context={(*ctx).clone()}>
         <BrowserRouter>
             <div class="flex w-screen h-screen">
                 <Switch<Route> render={Switch::render(switch)}/>
             </div>
         </BrowserRouter>
+        </ContextProvider<User>>
     }
 }
 
@@ -55,4 +75,5 @@ pub fn run_app() -> Result<(), JsValue> {
     yew::start_app::<Main>();
     Ok(())
 }
+
 
